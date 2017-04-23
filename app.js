044967f32035708ed421f85e2a6a4de7f0ad9466
clesire <clesiderata@gmail.com>
2017-04-23 13:14:17 -0400

    5.inherit_base_class

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
-//ES5
-// function TodoService() {}
-// TodoService.lastId = 0;
-// TodoService.getNextId = function() {
-//         return TodoService.lastId += 1;
-// }
-// TodoService.prototype.add = function(todo) {
-//     var newId = TodoService.getNextId();
-// }
-//ES6
-var TodoService = (function () {
-    function TodoService(todos) {
-        this.todos = todos;
-    }
-    TodoService.prototype.add = function (todo) {
-        var newId = TodoService.getNextId();
-    };
-    TodoService.prototype.getAll = function () {
-        return this.todos;
-    };
-    TodoService.getNextId = function () {
-        return TodoService.lastId += 1;
-    };
-    return TodoService;
-}());
-TodoService.lastId = 0;
 var TodoState;
 (function (TodoState) {
     TodoState[TodoState["New"] = 1] = "New";
@@ -33,46 +17,33 @@ var TodoState;
     TodoState[TodoState["Complete"] = 3] = "Complete";
     TodoState[TodoState["Deleted"] = 4] = "Deleted";
 })(TodoState || (TodoState = {}));
-//generally dont want to apply setter/getter to obj literal
-var todo = {
-    name: 'Pick up laundry',
-    get state() {
-        return this._state;
-    },
-    set state(newState) {
-        if (newState == TodoState.Complete) {
-            var canBeCompleted = this.state == TodoState.Active
-                || this.state == TodoState.Deleted;
-            if (!canBeCompleted) {
-                throw "State not Active|Deleted; Can't Complete";
-            }
+var TodoStateChanger = (function () {
+    function TodoStateChanger(newState) {
+        this.newState = newState;
+    }
+    //whether todo may be changed to new state
+    TodoStateChanger.prototype.canChangeState = function (todo) {
+        return !!todo;
+    };
+    TodoStateChanger.prototype.changeState = function (todo) {
+        if (this.canChangeState(todo)) {
+            todo.state = this.newState;
         }
-        this._state = newState;
-    }
-};
-todo.state = TodoState.Complete;
-//use class instead
-var SmartTodo = (function () {
-    function SmartTodo(name) {
-        this.name = name;
-    }
-    Object.defineProperty(SmartTodo.prototype, "state", {
-        get: function () {
-            return this._state;
-        },
-        set: function (newState) {
-            if (newState == TodoState.Complete) {
-                var canBeCompleted = this.state == TodoState.Active
-                    || this.state == TodoState.Deleted;
-                if (!canBeCompleted) {
-                    throw "State not Active|Deleted; Can't Complete";
-                }
-            }
-            this._state = newState;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return SmartTodo;
+        return todo;
+    };
+    return TodoStateChanger;
 }());
-var smartTodo = new SmartTodo('pick up laundry');
+var CompleteTodoStateChanger = (function (_super) {
+    __extends(CompleteTodoStateChanger, _super);
+    function CompleteTodoStateChanger() {
+        return _super.call(this, TodoState.Complete) || this;
+    }
+    CompleteTodoStateChanger.prototype.canChangeState = function (todo) {
+        //without overrideing all the logic; just extend
+        //continue to access methods defined in base
+        //even though it is being overriden
+        return _super.prototype.canChangeState.call(this, todo) && (todo.state == TodoState.Active
+            || todo.state == TodoState.Deleted);
+    };
+    return CompleteTodoStateChanger;
+}(TodoStateChanger));
--- app.ts
+++ app.ts
@@ -1,27 +1,4 @@
 export {};
-//ES5
-// function TodoService() {}
-// TodoService.lastId = 0;
-// TodoService.getNextId = function() {
-//         return TodoService.lastId += 1;
-// }
-// TodoService.prototype.add = function(todo) {
-//     var newId = TodoService.getNextId();
-// }
-//ES6
-class TodoService {
-    static lastId: number = 0;
-    constructor(private todos: Todo[]) {}
-    add(todo: Todo) {
-        var newId = TodoService.getNextId();
-    }
-    getAll() {
-        return this.todos;
-    }
-    static getNextId(): number {
-        return TodoService.lastId += 1;
-    }
-}
 interface Todo {
     name:string;
     state:TodoState;
@@ -30,45 +7,30 @@ enum TodoState {
     New = 1,
     Active, Complete, Deleted
 }
-//generally dont want to apply setter/getter to obj literal
-var todo = {
-    name: 'Pick up laundry',
-    get state() {
-        return this._state;
-    },
-    set state(newState){
-        if (newState == TodoState.Complete){
-            var canBeCompleted = 
-                this.state == TodoState.Active
-                || this.state == TodoState.Deleted;
-            if (!canBeCompleted){
-                throw "State not Active|Deleted; Can't Complete";
-            }
+class TodoStateChanger {
+    constructor(private newState: TodoState) {}
+    //whether todo may be changed to new state
+    canChangeState(todo: Todo): boolean {
+        return !!todo;
+    }
+    changeState(todo: Todo): Todo {
+        if(this.canChangeState(todo)) {
+            todo.state = this.newState;
         }
-        this._state = newState;
+        return todo;
     }
 }
-todo.state = TodoState.Complete;
-//use class instead
-class SmartTodo {
-    _state: TodoState;
-    name: string;
-    get state() {
-        return this._state;
+class CompleteTodoStateChanger extends TodoStateChanger {
+    constructor() {
+        super(TodoState.Complete);
     }
-    set state(newState){
-        if (newState == TodoState.Complete){
-            var canBeCompleted = 
-                this.state == TodoState.Active
-                || this.state == TodoState.Deleted;
-            if (!canBeCompleted){
-                throw "State not Active|Deleted; Can't Complete";
-            }
-        }
-        this._state = newState;
-    }
-    constructor(name:string){
-        this.name = name;
+    canChangeState(todo: Todo): boolean {
+        //without overrideing all the logic; just extend
+        //continue to access methods defined in base
+        //even though it is being overriden
+        return super.canChangeState(todo) && (
+            todo.state == TodoState.Active
+            || todo.state == TodoState.Deleted
+        )
     }
 }
-var smartTodo = new SmartTodo('pick up laundry');
