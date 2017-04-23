56551411a9fcea8c2f1648348ff3ad4e836a8b04
clesire <clesiderata@gmail.com>
2017-04-23 08:32:47 -0400

    4.interface_to_describe_fxn

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//var todo = <Todo>{};
-var todo = {
-    name: "pick up"
+//casting overrides TypeScript's type-checking
+var $ = function (selector) {
+    //Find DOM element
 };
+$.version = 1.12;
+var element = $('#container');
--- app.ts
+++ app.ts
@@ -1,23 +1,15 @@
 export {};
-//disambiguation for whether a file
-//= module (and have its own scope)
-//= script (and share the global scope with other scripts).
-//TS's solution=to simply state that a module is any file
-//which contains an import or export.
+interface jQuery {
+    (select: string): HTMLElement;
+    version: number;
+}
+//casting overrides TypeScript's type-checking
+var $ = <jQuery>function(selector) {
+    //Find DOM element
+}
+$.version = 1.12;
 
-//data structure
-interface Todo {
-    name: string;
-    completed?: boolean; //optional
-}
-//service
-interface ITodoService {
-    add(todo: Todo): Todo;
-    delete(todoId: number): void;
-    getAll(): Todo[];
-    getById(todoId: number): Todo;
-}
-//var todo = <Todo>{};
-let todo: Todo = {
-    name: "pick up"
-};
+var element = $('#container')
+
+
+
