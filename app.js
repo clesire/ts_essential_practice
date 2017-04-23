7dd2ab50c3f881b24a235027b2a2dbadb6cd9c77
clesire <clesiderata@gmail.com>
2017-04-23 14:37:48 -0400

    6.create_generics

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-//intention: return type should be same as of 'value'
-//since parse accepts a string to represent 
-//an object of any type, TS can't guess the type
-// function clone(value) {
-//     let serialized = JSON.stringify(value);
-//     return JSON.parse(serialized);
-// }
-function clone(value) {
-    var serialized = JSON.stringify(value);
-    return JSON.parse(serialized);
-}
-clone('hello');
-clone(123);
-var todo = {
-    id: 1,
-    name: 'pickup laundry',
-    state: TodoState.Active
-};
-clone(todo);
-//even work with object literal
-clone({ name: 'emptyname' });
+var array = [1, 2, 3];
+var array2 = [1, 2, 3];
+var KeyValuePair = (function () {
+    function KeyValuePair(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+    return KeyValuePair;
+}());
+var pair1 = new KeyValuePair(1, 'first');
+var pair2 = new KeyValuePair('second', new Date(Date.now()));
+var pair3 = new KeyValuePair(3, 'third');
+var KeyValuePairPrinter = (function () {
+    function KeyValuePairPrinter(pairs) {
+        this.pairs = pairs;
+    }
+    KeyValuePairPrinter.prototype.print = function () {
+        for (var _i = 0, _a = this.pairs; _i < _a.length; _i++) {
+            var p = _a[_i];
+            console.log(p.key + ":" + p.value);
+        }
+    };
+    return KeyValuePairPrinter;
+}());
+var printer = new KeyValuePairPrinter([pair1, pair3]);
+//pair2 does not share the same generic parameters 
+//thus different type of object
+//var printer=new KeyValuePairPrinter([pair1,pair2])
+printer.print();
--- app.ts
+++ app.ts
@@ -1,21 +1,29 @@
-//intention: return type should be same as of 'value'
-//since parse accepts a string to represent 
-//an object of any type, TS can't guess the type
-// function clone(value) {
-//     let serialized = JSON.stringify(value);
-//     return JSON.parse(serialized);
-// }
-function clone<T>(value: T): T {
-    let serialized = JSON.stringify(value);
-    return JSON.parse(serialized);
+var array: number[] = [1,2,3];
+var array2: Array<number> =[1,2,3];
+
+class KeyValuePair<TKey, TValue>{
+    constructor(
+        public key: TKey,
+        public value: TValue
+    ){}
 }
-clone('hello')
-clone(123)
-var todo: Todo = {
-    id:1, 
-    name: 'pickup laundry',
-    state: TodoState.Active
-};
-clone(todo);
-//even work with object literal
-clone({name:'emptyname'})
+let pair1
+    =new KeyValuePair<number,string>(1,'first');
+let pair2
+    =new KeyValuePair<string,Date>('second', new Date(Date.now()));
+let pair3
+    =new KeyValuePair<number,string>(3,'third');
+
+class KeyValuePairPrinter<T,U> {
+    constructor(private pairs: KeyValuePair<T,U>[]){}
+    print() {
+        for (let p of this.pairs) {
+            console.log(`${p.key}:${p.value}`)
+        }
+    }
+}
+var printer=new KeyValuePairPrinter([pair1,pair3])
+//pair2 does not share the same generic parameters 
+//thus different type of object
+//var printer=new KeyValuePairPrinter([pair1,pair2])
+printer.print();
