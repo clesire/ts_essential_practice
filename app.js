876290bb57ce7df4026596b4b0a9a5d531382dcc
clesire <clesiderata@gmail.com>
2017-04-23 14:26:11 -0400

    6.use_generic

--- app.js
+++ app.js
@@ -1,87 +1,21 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var TodoService = (function () {
-    function TodoService(todos) {
-        this.todos = todos;
-    }
-    Object.defineProperty(TodoService.prototype, "nextId", {
-        get: function () {
-            return TodoService.getNextId();
-        },
-        set: function (nextId) {
-            TodoService._lastId = nextId - 1;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    TodoService.prototype.add = function (todo) {
-        todo.id = this.nextId;
-        this.todos.push(todo);
-        return todo;
-    };
-    TodoService.prototype.getAll = function () {
-        //actual todo items exposed to manipulation
-        //return this.todos;
-        //performant way to clone objects
-        var clone = JSON.stringify(this.todos);
-        return JSON.parse(clone);
-    };
-    TodoService.getNextId = function () {
-        return TodoService._lastId += 1;
-    };
-    TodoService.prototype.getById = function (todoId) {
-        var filtered = this.todos.filter(function (x) { return x.id == todoId; });
-        if (filtered.length) {
-            return filtered[0];
-        }
-        return null;
-    };
-    TodoService.prototype.delete = function (todoId) {
-        var toDelete = this.getById(todoId);
-        var deletedIndex = this.todos.indexOf(toDelete);
-        this.todos.splice(deletedIndex, 1);
-    };
-    return TodoService;
-}());
-TodoService._lastId = 0;
-var TodoStateChanger = (function () {
-    function TodoStateChanger(newState) {
-        this.newState = newState;
-    }
-    TodoStateChanger.prototype.changeState = function (todo) {
-        if (this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-        return todo;
-    };
-    return TodoStateChanger;
-}());
-//new TodoStateChanger();
-var CompleteTodoStateChanger = (function (_super) {
-    __extends(CompleteTodoStateChanger, _super);
-    function CompleteTodoStateChanger() {
-        return _super.call(this, TodoState.Complete) || this;
-    }
-    CompleteTodoStateChanger.prototype.canChangeState = function (todo) {
-        //without overrideing all the logic; just extend
-        //continue to access methods defined in base
-        //even though it is being overriden
-        return !!todo && (todo.state == TodoState.Active
-            || todo.state == TodoState.Deleted);
-    };
-    return CompleteTodoStateChanger;
-}(TodoStateChanger));
-var SmartTodo = (function () {
-    function SmartTodo(name) {
-        this.name = name;
-    }
-    return SmartTodo;
-}());
+//intention: return type should be same as of 'value'
+//since parse accepts a string to represent 
+//an object of any type, TS can't guess the type
+// function clone(value) {
+//     let serialized = JSON.stringify(value);
+//     return JSON.parse(serialized);
+// }
+function clone(value) {
+    var serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
+}
+clone('hello');
+clone(123);
+var todo = {
+    id: 1,
+    name: 'pickup laundry',
+    state: TodoState.Active
+};
+clone(todo);
+//even work with object literal
+clone({ name: 'emptyname' });
--- app.ts
+++ app.ts
@@ -1,72 +1,21 @@
-interface IIdGenerator {
-    nextId: number;
+//intention: return type should be same as of 'value'
+//since parse accepts a string to represent 
+//an object of any type, TS can't guess the type
+// function clone(value) {
+//     let serialized = JSON.stringify(value);
+//     return JSON.parse(serialized);
+// }
+function clone<T>(value: T): T {
+    let serialized = JSON.stringify(value);
+    return JSON.parse(serialized);
 }
-
-class TodoService implements ITodoService, IIdGenerator {
-    private static _lastId: number = 0;
-    get nextId() {
-        return TodoService.getNextId();
-    }
-    set nextId(nextId){
-        TodoService._lastId = nextId - 1;
-    }
-    constructor(private todos: Todo[]) {}
-    add(todo: Todo): Todo {
-        todo.id = this.nextId;
-        this.todos.push(todo);
-        return todo;
-    }
-    getAll(): Todo[] {
-        //actual todo items exposed to manipulation
-        //return this.todos;
-        //performant way to clone objects
-        var clone = JSON.stringify(this.todos);
-        return JSON.parse(clone);
-    }
-    static getNextId(): number {
-        return TodoService._lastId += 1;
-    }
-    getById(todoId: number):Todo {
-        var filtered = 
-            this.todos.filter(x=>x.id == todoId);
-        if (filtered.length) {
-            return filtered[0];
-        }
-        return null;
-    }
-    delete(todoId: number): void {
-        var toDelete = this.getById(todoId);
-        var deletedIndex = this.todos.indexOf(toDelete);
-        this.todos.splice(deletedIndex,1);
-    }
-}
-abstract class TodoStateChanger {
-    constructor(protected newState: TodoState) {}
-    //whether todo may be changed to new state
-    abstract canChangeState(todo: Todo): boolean;
-
-    changeState(todo: Todo): Todo {
-        if(this.canChangeState(todo)) {
-            todo.state = this.newState;
-        }
-        return todo;
-    }
-}
-//new TodoStateChanger();
-class CompleteTodoStateChanger extends TodoStateChanger {
-    constructor() {
-        super(TodoState.Complete);
-    }
-    canChangeState(todo: Todo): boolean {
-        //without overrideing all the logic; just extend
-        //continue to access methods defined in base
-        //even though it is being overriden
-        return !!todo && (
-            todo.state == TodoState.Active
-            || todo.state == TodoState.Deleted
-        )
-    }
-}
-class SmartTodo {
-    constructor(public name: string) {}
-}
+clone('hello')
+clone(123)
+var todo: Todo = {
+    id:1, 
+    name: 'pickup laundry',
+    state: TodoState.Active
+};
+clone(todo);
+//even work with object literal
+clone({name:'emptyname'})
