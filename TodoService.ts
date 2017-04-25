cb5cc6f39645ffe0544af9590c1a66eea81d448e
clesire <clesiderata@gmail.com>
2017-04-25 16:54:29 -0400

    9.method_decorator

--- TodoService.ts
+++ TodoService.ts
@@ -28,6 +28,7 @@ export default class TodoService implements ITodoService {
     //accepts a todo name or todo object
     add(todo: Todo): Todo 
     add(todo: string): Todo
+    @log
     add (input): Todo {
         var todo: Todo ={
             id: generateTodoId(),
@@ -77,4 +78,35 @@ export default class TodoService implements ITodoService {
         }
         return null;
     }   
+}
+//to add the logging logic without changing the code
+//inside of the add method, wrap the original method
+//in another method.
+//It replaces the original method with another method 
+//by actually calling that original method, 
+//and then returning its result.
+var originalMethod = TodoService.prototype.add;
+TodoService.prototype.add = function(...args){
+
+    console.log(`add(${JSON.stringify(args)})`);
+    let returnValue = originalMethod.apply(this, args);
+    console.log(`add(${JSON.stringify(args)}) => ${returnValue}`);
+    return returnValue;
+}
+//target:the object that the member lives on.
+//e.g. an instance of a to do service
+//method name: the name of the method to be decorated
+//descriptor: an object that contains all of the meta data
+//for the method that you are looking to modify
+function log(target: Object, methodName:string, descriptor:
+TypedPropertyDescriptor<Function>) {
+    descriptor.value
+    var originalMethod = TodoService.prototype.add;
+TodoService.prototype.add = function(...args){
+
+    console.log(`${methodName}(${JSON.stringify(args)})`);
+    let returnValue = originalMethod.apply(this, args);
+    console.log(`${methodName}(${JSON.stringify(args)}) => ${returnValue}`);
+    return returnValue;
+}
 }
